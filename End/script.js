0eff53056599e34b156abd119fca3fa928029e8f
Z-iutek <127032295+Z-iutek@users.noreply.github.com>
2024-02-05 20:50:59 +0100

    Add files via upload

---
+++ End/script.js
@@ -0,0 +1,17 @@
+document.getElementById('reset-button').addEventListener('click', function() {
+    
+    // Tutaj dodaj kod do przejścia do inicjalizacji gry.
+    window.location.href = '../index.html';
+    console.log('Rozpocznij grę!');
+});
+   
+document.addEventListener('DOMContentLoaded', function(){
+
+    const highlightedMoney = localStorage.getItem('highlightedMoney');
+    console.log('Odczytano wartość z Local Storage:', highlightedMoney);
+
+    const moneyDisplay = document.getElementById('wynik')
+    moneyDisplay.textContent = `Your score is ${highlightedMoney} points`;
+
+})   
+
